b4985e7cca809ef2a17fcca319a2d956a93ccefc
Shakana Fox <foxshakana@gmail.com>
2020-11-03 11:18:35 -0500

    removed index.html, updated readme layuot in index.js, changed index file name to test.html

--- index.js
+++ index.js
@@ -32,11 +32,10 @@ inquirer.prompt ([
 }
 ]).then(response =>{
     console.log(response);
-})
-// const questions = [
-//       "What is your name?", "What is the title?", "What are the links?", "What is your URL?", "What is your email?"
+    const questions = [
+      "What is your name?", "What is the title?", "What are the links?", "What is your URL?", "What is your email?"
 
-// ];
+];
  
 const htmlTemplate = `<!DOCTYPE html>
 
@@ -50,28 +49,31 @@ const htmlTemplate = `<!DOCTYPE html>
         <link rel="stylesheet" href="">
     </head>
     <body>
-      <heading>#${response.title}</heading>
+      <heading>#${response.title} by ${response.name} email: ${response.linkEmail}</heading>
 
       <h2># Description</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, sapiente omnis deleniti harum architecto veritatis, adipisci ullam illo, explicabo aliquid voluptatum eos tempora cumque dignissimos at doloribus recusandae. Iste, saepe.</p>
          <ul>
-           <li><a href="https://github.com/laShawnfox/readme-generator${response.githubLink}></a></li>
-           <li><a href="https://lashawnfox.github.io/readme-generator/${response.githubURL}></a></li>
-         </ul>
+           <li><a href="https://github.com/laShawnfox/readme-generator${response.githubLink}>Github</a></li>
+           <li><a href="https://lashawnfox.github.io/readme-generator/${response.githubURL}></a>Github Link</li>
+          </ul>
         <br>
-      <h2>Table of Contents</h2>
-      <p><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">INSTALLATION</a>
-       <br>
-       <a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">USAGE</a>
-       <br>
-       <a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">LICENSE</a>
-       <br>
-       <a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">CONTRIBUTING</a>
-       <br>
-       <a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">TESTS</a>
-       <br>
-       <a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">QUESTIONS</a>              
-      </p>
+      <h2>#Table of Contents</h2>
+      <p>
+        <ul>
+         <li><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">INSTALLATION</a></li> 
+      
+         <li><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">USAGE</a></li> 
+       
+         <li><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">LICENSE</a></li> 
+      
+         <li><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">CONTRIBUTING</a></li> 
+       
+         <li><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">TESTS</a></li> 
+       
+         <li><a href="https://github.com/laShawnfox/readme-generator/blob/main/README.md">QUESTIONS</a> </li>              
+         </ul>
+       </p>
       <br>
       <h2> # Installation</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, sapiente omnis deleniti harum architecto veritatis, adipisci ullam illo, explicabo aliquid voluptatum eos tempora cumque dignissimos at doloribus recusandae. Iste, saepe.</p>
@@ -90,15 +92,21 @@ const htmlTemplate = `<!DOCTYPE html>
       <br>
       <h2> # Questions</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, sapiente omnis deleniti harum architecto veritatis, adipisci ullam illo, explicabo aliquid voluptatum eos tempora cumque dignissimos at doloribus recusandae. Iste, saepe.</p>
- 
-        <script src="" async defer></script>
     </body>
 </html>`;
 // function to write README file
 fs.writeFile("index.html",htmlTemplate, "utf8", (err) =>{
     if(err) throw(err);
     console.log("Success Readme!");
+});
 })
+// const questions = [
+//       "What is your name?", "What is the title?", "What are the links?", "What is your URL?", "What is your email?"
+
+// ];
+ 
+
+
 // function writeToFile(fileName, data) {
 
 // }
